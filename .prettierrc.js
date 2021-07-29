3140bba975aae4e985b4a0481b79eacdf1046dbf
Noah Bergamo <noah.bergamo@hotmail.com>
2021-07-29 20:36:20 -0300

    Initial Setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  bracketSpacing: true,
+  quoteProps: 'consistent' 
+
+};
---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      <Text> This is my app.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400'
+  },
+  highlight: {
+    fontWeight: '700'
+  }
+});
+
+export default App;
